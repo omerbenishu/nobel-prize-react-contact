8878bef197cdcf197d0c99fe879d2e640b223fd1
omerbenishu <omer.benishu@gmail.com>
2022-11-07 13:12:29 +0200

    Get data and show title and description

--- index.js
+++ index.js
@@ -7,14 +7,71 @@
 // notice in our html we have a node with ID "app"
 // hint: use this reference later to inject data into your page
 const app = document.getElementById('app');
-
+const apiPrefix = "https://api.nobelprize.org/2.1/laureates"
+var currentQuery = ''
+const titleType = 'h2'
+const descType = 'p'
+const dataClassName = 'data'
+const itemClassName = 'item'
+var lang = 'en'
 async function getData() {
   // write you logic for getting the data from the API here
   // return your data from this function
+  fetch(apiPrefix + currentQuery).
+  then(data => data.json()).
+  then(data => render(data))
+  
+
 }
 
-async function render() {
-  data = await getData();
-  // you have your data! add logic here to render it to the UI
-  // notice in the HTML file we call render();
+async function render(data) {
+    var laureatesArray = data['laureates']
+    console.log(laureatesArray[0])
+    
+    
+    // Filtering
+
+    // Sorting
+
+    // wiki image https://en.wikipedia.org/w/api.php?action=query&format=json&formatversion=2&prop=pageimages|pageterms&piprop=thumbnail&pithumbsize=500&titles=Albert Einstein
+
+    laureatesArray.forEach(element => {
+
+      laureateName = document.createElement(titleType)
+      laureateName.innerHTML = element['fullName'][lang]
+
+      laureateDesc = document.createElement(descType)
+      nobelPrizes = element['nobelPrizes']
+      var desc = ''
+      nobelPrizes.forEach(nobel => {
+        desc += 'Category: ' + nobel['category'][lang] + '\n' + nobel['motivation'][lang]
+      })
+      laureateDesc.innerText = desc
+      
+      //const img = document.createElement('img')
+      //img.src = 'https://en.wikipedia.org/w/api.php?action=query&prop=pageimages&titles=Jaguar&pithumbsize=500'
+
+
+
+      const card = document.createElement('div');
+      card.className = dataClassName;
+      card.append(laureateName, laureateDesc)
+
+      const presenter = document.createElement('div');
+      presenter.className = itemClassName;
+      presenter.append(card);
+
+      document.getElementById('app').append(presenter);
+    });
+
 }
+
+async function getWikiImage(laureateData){
+  fetch('https://www.wikidata.org/wiki/Special:EntityData/Q173500.json').
+  then(data => data.json()).
+  then(data => console.log(data))
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  getData();
+})
