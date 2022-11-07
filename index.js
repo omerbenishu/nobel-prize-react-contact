c3458288c3930a696288a4e9fcb8f50e8f034bb1
Omer Benishu <omer.benishu@kryonsystems.com>
2022-11-07 10:24:21 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+//////////////
+// we have a basic skeleton here to help you start.
+// if you dont want to use it you dont have to -
+// just clear the file and start from scratch
+//////////////
+
+// notice in our html we have a node with ID "app"
+// hint: use this reference later to inject data into your page
+const app = document.getElementById('app');
+
+async function getData() {
+  // write you logic for getting the data from the API here
+  // return your data from this function
+}
+
+async function render() {
+  data = await getData();
+  // you have your data! add logic here to render it to the UI
+  // notice in the HTML file we call render();
+}
