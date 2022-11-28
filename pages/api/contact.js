5ac20dfbacbe0e6b2712afb63f937146b2e4bf28
omerbenishu <omer.benishu@gmail.com>
2022-11-28 14:01:43 +0200

    fix secret

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -2,7 +2,6 @@ import { CONTACT_FORM_TABLE_NAME } from "../../config";
 import { client } from "../../datastore";
 
 export default async function handler(req, res) {
-
   // Don't allow methods we don't support
   if (req.method !== 'POST') {
     res.status(405).send({ message: 'Unsupported request method' })
@@ -14,9 +13,14 @@ export default async function handler(req, res) {
 
   // write our data to the database
   try {
-    await client.from(CONTACT_FORM_TABLE_NAME).insert({ name, category, reason });
+    const { error } = await client.from(CONTACT_FORM_TABLE_NAME).insert({ name, category, reason });
+    if (error) {
+        console.log(error)
+        res.status(500).json({ message: error });
+        return;
+    }
     res.status(200).json({message: "Success"})
   } catch (error) {
-    res.status(400).json({ message: "Something went wrong" })
+    res.status(400).json({ message: `Something went wrong: ${error}` })
   }
 }
